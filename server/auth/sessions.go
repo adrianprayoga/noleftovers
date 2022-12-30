1ab9f6382a269f5a92b1859ef287167c4c3dd902
Adrian <adrian.prayoga@outlook.com>
2022-12-30 21:34:11 +0800

    Use configured key for sessions

--- server/auth/sessions.go
+++ server/auth/sessions.go
@@ -2,13 +2,15 @@ package auth
 
 import (
 	"github.com/gorilla/sessions"
+	"github.com/spf13/viper"
 )
 
 var Store *sessions.CookieStore
+
 //= sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
 
 func InitSessions() {
-	key := "key"// securecookie.GenerateRandomKey(32)
+	key := viper.GetString("session_key")
 
 	//logger.Log.Info("cookie",zap.ByteString("cookie", key))
 	Store = sessions.NewCookieStore([]byte(key))
